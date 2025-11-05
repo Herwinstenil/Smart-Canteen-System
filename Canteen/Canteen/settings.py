d2590ef95b42b487cd501ea7bf02d830ad2f8f30
Herwinstenil <herwinstenil24@gmail.com>
2025-11-05 14:54:06 +0530

    Smart

--- Canteen/Canteen/settings.py
+++ Canteen/Canteen/settings.py
@@ -109,8 +109,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Your Mail id'
-EMAIL_HOST_PASSWORD = 'Your Password'
+EMAIL_HOST_USER = 'herwinstenil24@gmail.com'
+EMAIL_HOST_PASSWORD = 'qguw fsye evzk xjjj'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
